3c4e1707f82bffd675d9c82cf2d05d6d5ffb9723
Jacques Morin <38377390+adoprix@users.noreply.github.com>
2023-02-08 11:47:54 +0100

    Add files via upload

---
+++ aes-application/src/main.h
@@ -0,0 +1,18 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <irq.h>
+#include <libbase/uart.h>
+#include <libbase/console.h>
+#include <generated/csr.h>
+
+#include <mbedtls/gcm.h>
+#include <mbedtls/cipher.h>
+#include <tinycrypt/ctr_prng.h>
+
+#endif
