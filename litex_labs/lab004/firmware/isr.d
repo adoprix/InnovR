743b518e7d5905fbb04a7ab04ea1da87e9f3434c
jamorin <jacques.morin@insa-rennes.fr>
2023-02-14 13:45:41 +0100

    modif des ressources

--- litex_labs/lab004/firmware/isr.d
+++ litex_labs/lab004/firmware/isr.d
@@ -1,27 +1,40 @@
 isr.o: isr.c \
- /home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/generated/csr.h \
- /home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/generated/soc.h \
- /home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
- /home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
- /home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
- /home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/../libc/picolibc.h \
- /home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
- /home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
- /home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/system.h \
- /home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
- /home/jamorin/litex_root/litex/litex/soc/software/include/hw/common.h \
- /home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/irq.h \
- /home/jamorin/litex_root/litex/litex/soc/software/libbase/uart.h
-/home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/generated/csr.h:
-/home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/generated/soc.h:
-/home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
-/home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
-/home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
-/home/jamorin/InnovR_litex/litex_labs/lab004/build/software/include/../libc/picolibc.h:
-/home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
-/home/jamorin/litex_root/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
-/home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/system.h:
-/home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
-/home/jamorin/litex_root/litex/litex/soc/software/include/hw/common.h:
-/home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/irq.h:
-/home/jamorin/litex_root/litex/litex/soc/software/libbase/uart.h:
+ /home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/generated/csr.h \
+ /home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/generated/soc.h \
+ /home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/../libc/picolibc.h \
+ /home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/system.h \
+ /home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h \
+ /home/mpelcat/Projets/litex/litex/litex/soc/software/include/hw/common.h \
+ /home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/irq.h \
+ /home/mpelcat/Projets/litex/litex/litex/soc/software/libbase/uart.h
+
+/home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/generated/csr.h:
+
+/home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/generated/soc.h:
+
+/home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+
+/home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+
+/home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+
+/home/mpelcat/Projets/InnovR/litex_labs/lab004/build/software/include/../libc/picolibc.h:
+
+/home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+
+/home/mpelcat/Projets/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+
+/home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/system.h:
+
+/home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/csr-defs.h:
+
+/home/mpelcat/Projets/litex/litex/litex/soc/software/include/hw/common.h:
+
+/home/mpelcat/Projets/litex/litex/litex/soc/cores/cpu/vexriscv/irq.h:
+
+/home/mpelcat/Projets/litex/litex/litex/soc/software/libbase/uart.h:
