7639ae7d3f967d02555a710b632174289711b61f
Morin Jacques <jacques.morin@insa-rennes.fr>
2023-03-22 13:34:56 +0100

    matinée 22/03

--- firmware_gap8/main.c
+++ firmware_gap8/main.c
@@ -9,95 +9,6 @@ uint8_t nonce[NONCE_SIZE];
 TCCtrPrng_t ctx;
 
 
-/*-----------------------------------------------------------------------*/
-/* Uart                                                                  */
-/*-----------------------------------------------------------------------*/
-
-static char *readstr(void)
-{
-	char c[2];
-	static char s[64];
-	static int ptr = 0;
-
-	if(readchar_nonblock()) {
-		c[0] = readchar();
-		c[1] = 0;
-		switch(c[0]) {
-			case 0x7f:
-			case 0x08:
-				if(ptr > 0) {
-					ptr--;
-					putsnonl("\x08 \x08");
-				}
-				break;
-			case 0x07:
-				break;
-			case '\r':
-			case '\n':
-				s[ptr] = 0x00;
-				putsnonl("\n");
-				ptr = 0;
-				return s;
-			default:
-				if(ptr >= (sizeof(s) - 1))
-					break;
-				putsnonl(c);
-				s[ptr] = c[0];
-				ptr++;
-				break;
-		}
-	}
-
-	return NULL;
-}
-
-static char *get_token(char **str)
-{
-	char *c, *d;
-
-	c = (char *)strchr(*str, ' ');
-	if(c == NULL) {
-		d = *str;
-		*str = *str+strlen(*str);
-		return d;
-	}
-	*c = 0;
-	d = *str;
-	*str = c+1;
-	return d;
-}
-
-static void prompt(void)
-{
-	printf("RUNTIME>");
-}
-
-/*-----------------------------------------------------------------------*/
-/* Help                                                                  */
-/*-----------------------------------------------------------------------*/
-
-static void help(void)
-{
-	puts("Available commands:");
-	puts("help                            - this command");
-	puts("reboot                          - reboot CPU");
-	puts("display                         - display test");
-	puts("led                             - led test");
-	puts("ledreal                         - real led test");
-	puts("switches                        - switches test");
-	puts("knight                          - knight rideeeeeers");
-	puts("encrypt			             - encrypt test");
-}
-
-/*-----------------------------------------------------------------------*/
-/* Commands                                                              */
-/*-----------------------------------------------------------------------*/
-
-static void reboot(void)
-{
-	ctrl_reset_write(1);
-}
-
 /**
  * @brief Get the hex representation of the input string
  * 
@@ -138,57 +49,6 @@ static uint8_t get_hex_rep(char *str_input, uint8_t in_size, uint8_t *hex_out)
 }
 
 
-static void display_test(void)
-{
-	int i;
-	printf("display_test...\n");
-	for(i=0; i<6; i++) {
-		display_sel_write(i);
-		display_value_write(i);
-		display_write_write(1);
-	}
-}
-
-static void led_test(void)
-{
-	int i;
-	printf("led_test...\n");
-	for(i=0; i<32; i++) {
-		leds_out_write(i);
-		busy_wait(1);
-	}
-}
-
-static void led_test_extended(void)
-{
-	int i;
-	printf("led_test_extended...\n");
-	for(i=0; i<1024; i++) {
-		leds_out_write(i);
-		busy_wait(20);
-	}
-}
-
-static void switches_test(void) {
-    printf("switches_test...\n");
-    leds_out_write(switches_in_read());
-    // voir build/software/include/generated/csr.h pour toutes les fonctions d'accès aux registres
-}
-
-static void knight_riderrrrrrr(void) {
-    printf("knight_riderrrrrrr !!\n");
-    for(int i=0 ; i<10 ; i++) {
-        int j;
-        for(j=0 ; j<10 ; j++){
-            leds_out_write(0x1<<j);
-            busy_wait(50);
-        }
-        for(j=8 ; j>0 ; j--){
-            leds_out_write(0x1<<j);
-            busy_wait(50);
-        }
-    }
-}
 
 /*-----------------------------------------------------------------------*/
 /* Encryption/Decryption                                                 */
@@ -376,30 +236,17 @@ static void console_service(void)
 
 	str = readstr();
 	if(str == NULL) return;
+	
 	token = get_token(&str);
-	if(strcmp(token, "help") == 0)
-		help();
-	else if(strcmp(token, "reboot") == 0)
-		reboot();
-	else if(strcmp(token, "display") == 0)
-		display_test();
-	else if(strcmp(token, "led") == 0)
-		led_test();
-	else if(strcmp(token, "ledreal") == 0)
-		led_test_extended();
-	else if(strcmp(token, "switches") == 0)
-	    switches_test();
-	else if(strcmp(token, "knight") == 0)
-	    knight_riderrrrrrr();
+	
 	else if(strcmp(token, "encrypt") == 0) {
 	    uint8_t num = 4;
 	    encrypts(&num, 1);
 	    decrypts();
 	}
-	prompt();
 }
 
-int main(int argc, char* argv[])
+int main(void)
 {
 #ifdef CONFIG_CPU_HAS_INTERRUPT
 	irq_setmask(0);
