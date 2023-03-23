35bb3ae811ee105e056fbc03d707cb85b964821e
Morin Jacques <jacques.morin@insa-rennes.fr>
2023-03-23 17:33:28 +0100

    ça fonctionne

---
+++ monocore_aes/firmware/crt0.d
@@ -0,0 +1,2 @@
+crt0.o: \
+ /home/jamorin/litex_root/litex/litex/soc/cores/cpu/vexriscv/crt0.S
--- monocore_aes/firmware/main.c
+++ monocore_aes/firmware/main.c
@@ -1,24 +1,14 @@
 // This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
-// Modified by Joseph Faye
-// Modified by Lucas Esteves <lucas.esteves-rocha@insa-rennes.fr>
 // License: BSD
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "aes.h"
-#include "amp_comms.h"
-#include "amp_utils.h"
-
 #include <irq.h>
 #include <libbase/uart.h>
 #include <libbase/console.h>
 #include <generated/csr.h>
-
-amp_comms_tx_t _tx; // A enlever
-amp_comms_rx_t _rx; // A enlever
-private_aes_data_t priv_data;// A enlever
 
 /*-----------------------------------------------------------------------*/
 /* Uart                                                                  */
@@ -80,7 +70,7 @@ static char *get_token(char **str)
 
 static void prompt(void)
 {
-    printf("\e[92;1mCore1-console\e[0m> ");
+    printf("\e[92;1mlitex-demo-app\e[0m> ");
 }
 
 /*-----------------------------------------------------------------------*/
@@ -89,10 +79,18 @@ static void prompt(void)
 
 static void help(void)
 {
-    puts("\nAES app built "__DATE__" "__TIME__"\n");
+    puts("\nLiteX minimal demo app built "__DATE__" "__TIME__"\n");
     puts("Available commands:");
     puts("help               - Show this command");
     puts("reboot             - Reboot CPU");
+#ifdef CSR_LEDS_BASE
+    puts("led                - Led demo");
+#endif
+    puts("donut              - Spinning Donut demo");
+    puts("helloc             - Hello C");
+#ifdef WITH_CXX
+    puts("hellocpp           - Hello C++");
+#endif
 }
 
 /*-----------------------------------------------------------------------*/
@@ -103,6 +101,38 @@ static void reboot_cmd(void)
 {
     ctrl_reset_write(1);
 }
+
+#ifdef CSR_LEDS_BASE
+static void led_cmd(void)
+{
+	int i;
+	printf("Led demo...\n");
+
+	printf("Counter mode...\n");
+	for(i=0; i<32; i++) {
+		leds_out_write(i);
+		busy_wait(100);
+	}
+
+	printf("Shift mode...\n");
+	for(i=0; i<4; i++) {
+		leds_out_write(1<<i);
+		busy_wait(200);
+	}
+	for(i=0; i<4; i++) {
+		leds_out_write(1<<(3-i));
+		busy_wait(200);
+	}
+
+	printf("Dance mode...\n");
+	for(i=0; i<4; i++) {
+		leds_out_write(0x55);
+		busy_wait(200);
+		leds_out_write(0xaa);
+		busy_wait(200);
+	}
+}
+#endif
 
 /*-----------------------------------------------------------------------*/
 /* Console service / Main                                                */
@@ -120,7 +150,10 @@ static void console_service(void)
         help();
     else if(strcmp(token, "reboot") == 0)
         reboot_cmd();
-
+#ifdef CSR_LEDS_BASE
+        else if(strcmp(token, "led") == 0)
+		led_cmd();
+#endif
     prompt();
 }
 
@@ -131,110 +164,12 @@ int main(void)
 	irq_setie(1);
 #endif
     uart_init();
-    console_service();
+
     help();
-
-    amp_millis_init();
-    amp_aes_init(&priv_data);
-    amp_comms_init(&_tx, &_rx);
-
-    /* Initing nonce */
-    amp_aes_update_nonce(&priv_data);
-
     prompt();
-
-    const int MEASURE_STEPS = 100;
-    uint32_t t_aes_begin, t_aes_end, counter, t_send_begin,  t_send_end, t_receive_begin, t_receive_end;
-    double lat_aes_ms = 0;
-    double t_send_ms = 0;
-    double t_receive_ms = 0;
-    float time_spent_ms;
-    amp_cmds_t cmd_rx;
-    int img_size;
-    uint8_t sel_op, class_predicted;
-
-    counter = 0;
 
     while(1) {
         console_service();
-
-        /* Checking comunication data */
-        cmd_rx = amp_comms_has_unread(&_rx);
-
-        if(cmd_rx != AMP_NULL)
-        {
-            if(counter == 0)
-            {
-                printf("\n");
-            }
-
-            switch (cmd_rx)
-            {
-                case AMP_SEND_PREDICTION:
-                    t_receive_begin = amp_millis();
-                    amp_comms_receive(&_rx, &class_predicted, sizeof(class_predicted));
-                    t_receive_end = amp_millis();
-
-                    time_spent_ms = (t_receive_begin - t_receive_end)/(CONFIG_CLOCK_FREQUENCY/1000.0);
-                    t_receive_ms += time_spent_ms;
-
-                    sel_op = 1;
-                    break;
-
-                default:
-                    /* Blocking program, command not implemented */
-                    while(1);
-                    sel_op = 0;
-                    break;
-            }
-
-            if(sel_op  == 1){
-                
-                printf("Class received: %d - Measuring step: %lu/%d\r", class_predicted, counter+1, MEASURE_STEPS);          
-
-                t_aes_begin = amp_millis();
-
-                int result = 0;
-                result = amp_aes_update_nonce(&priv_data);
-                result = amp_aes_encrypts(&class_predicted, &priv_data);
-
-                t_aes_end = amp_millis();
-                time_spent_ms = (t_aes_begin - t_aes_end)/(CONFIG_CLOCK_FREQUENCY/1000.0);
-                lat_aes_ms += time_spent_ms;
-
-                counter++;
-
-                if (result != 0)
-                {
-                    printf("\e[91;1m\nError in the encryption. Err= %d\e[0m\n", result);
-                }
-            }
-        }
-
-        if(counter == MEASURE_STEPS)
-        {
-            counter = 0;
-
-            time_spent_ms = lat_aes_ms/MEASURE_STEPS;
-            int f_left = (int)time_spent_ms;
-            int f_right = ((float)(time_spent_ms - f_left)*1000.0);
-            printf("\nAES Latency for predicted class: %d is %d.%d ms\n", class_predicted, f_left, f_right);
-
-            time_spent_ms = t_send_ms/MEASURE_STEPS;
-            f_left = (int)time_spent_ms;
-            f_right = ((float)(time_spent_ms - f_left)*1000.0);
-            printf("Total Communication send is %d.%d ms\n", f_left, f_right);
-
-            time_spent_ms = t_receive_ms/MEASURE_STEPS;
-            f_left = (int)time_spent_ms;
-            f_right = ((float)(time_spent_ms - f_left)*1000.0);
-            printf("Total Communication receive is %d.%d ms\n", f_left, f_right);
-
-            t_receive_ms = 0;
-            t_send_ms = 0;
-            lat_aes_ms = 0;
-            prompt();
-        }
     }
 
     return 0;
